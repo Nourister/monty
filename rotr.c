3802505d7b159a6ce6e93ed47ff7f4bf517db03f
nourister <nourusterjuma@gmail.com>
2023-07-27 13:34:19 -0700

    monty

--- rotr.c
+++ rotr.c
@@ -1,24 +1,30 @@
 #include "monty.h"
 
-/* Function to rotate the stack to the bottom */
+/**
+ * rotr - rotates the stack to the bottom.
+ * @stack: double pointer to the top of the stack
+ * @line_number: line number of the current opcode
+ * Return: 0(success)
+ */
+
 void rotr(stack_t **stack, unsigned int line_number)
 {
-    stack_t *current = *stack;
-    stack_t *new_last = NULL;
+	stack_t *current = *stack;
+	stack_t *new_last = NULL;
 
-    if (current == NULL || current->next == NULL)
-        return; 
+	if (current == NULL || current->next == NULL)
+		return; 
 
-    while (current->next != NULL)
-    {
-        new_last = current;
-        current = current->next;
-    }
+	while (current->next != NULL)
+	{
+		new_last = current;
+		current = current->next;
+	}
 
-    new_last->next = NULL;
-    current->next = *stack;
-    (*stack)->prev = current;
-    *stack = current;
-    (void)line_number;
+	new_last->next = NULL;
+	current->next = *stack;
+	(*stack)->prev = current;
+	*stack = current;
+	(void)line_number;
 }
 
