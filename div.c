55e928210404aa54d3cb1c8fb4c379f0b4f6dfae
anyango-okoth <anyangomoureen02@gmail.com.com>
2023-07-27 12:21:58 -0700

    betty-fix_div

--- div.c
+++ div.c
@@ -1,21 +1,25 @@
 #include "monty.h"
 
-/* Function to divide the second top element by the top element on the stack */
+/**
+ * stack_div - Function to divide the second top element by the top element.
+ * @stack: Double pointer to the stack.
+ * @line_number: Line number in the file where the instruction appears.
+ */
 void stack_div(stack_t **stack, unsigned int line_number)
 {
-    if (*stack == NULL || (*stack)->next == NULL)
-    {
-        fprintf(stderr, "L%u: can't div, stack too short\n", line_number);
-        exit(EXIT_FAILURE);
-    }
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't div, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 
-    if ((*stack)->n == 0)
-    {
-        fprintf(stderr, "L%u: division by zero\n", line_number);
-        exit(EXIT_FAILURE);
-    }
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%u: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 
-    (*stack)->next->n /= (*stack)->n;
-    pop(stack, line_number);
+	(*stack)->next->n /= (*stack)->n;
+	pop(stack, line_number);
 }
 
