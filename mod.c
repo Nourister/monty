359e33cc7b7b593736d0d6da1a9c1dc8a6bd51a9
nourister <nourusterjuma@gmail.com>
2023-07-27 12:53:07 -0700

    monty

--- mod.c
+++ mod.c
@@ -1,21 +1,28 @@
 #include "monty.h"
 
-/* Function to compute the remainder of the division of the second top element by the top element on the stack */
+/**
+ * mod - Computes the remainder of the division of the second top element
+ *       of the stack by the top element of the stack.
+ * @stack: Double pointer to the head of the stack.
+ * @line_number: The line number.
+ * Return: 0(success)
+ */
+
 void mod(stack_t **stack, unsigned int line_number)
 {
-    if (*stack == NULL || (*stack)->next == NULL)
-    {
-        fprintf(stderr, "L%u: can't mod, stack too short\n", line_number);
-        exit(EXIT_FAILURE);
-    }
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't mod, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 
-    if ((*stack)->n == 0)
-    {
-        fprintf(stderr, "L%u: division by zero\n", line_number);
-        exit(EXIT_FAILURE);
-    }
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%u: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 
-    (*stack)->next->n %= (*stack)->n;
-    pop(stack, line_number);
+	(*stack)->next->n %= (*stack)->n;
+	pop(stack, line_number);
 }
 
