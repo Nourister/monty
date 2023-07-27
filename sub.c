26c2d818c633898706c4f7ecff098086f954b56b
anyango-okoth <anyangomoureen02@gmail.com.com>
2023-07-27 12:02:59 -0700

    fix_betty_sub

--- sub.c
+++ sub.c
@@ -1,15 +1,23 @@
 #include "monty.h"
 
-/* Function to subtract the top element from the second top element on the stack */
+/**
+ * sub - Function to subtract the top element from the second top element.
+ * @stack: Double pointer to the top of the stack.
+ * @line_number: Line number in the Monty file where the instruction appears.
+ *
+ * Return: None.
+ */
 void sub(stack_t **stack, unsigned int line_number)
 {
-    if (*stack == NULL || (*stack)->next == NULL)
-    {
-        fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
-        exit(EXIT_FAILURE);
-    }
+	/* Check if the stack is too short to perform subtraction */
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 
-    (*stack)->next->n -= (*stack)->n;
-    pop(stack, line_number);
+	/* Subtract the top element from the second top element */
+	(*stack)->next->n -= (*stack)->n;
+	pop(stack, line_number);
 }
 
