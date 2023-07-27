04ec9f5d134c21e6e7ace857fc53e78b2663406d
nourister <nourusterjuma@gmail.com>
2023-07-27 13:30:58 -0700

    monty

--- rotl.c
+++ rotl.c
@@ -1,21 +1,27 @@
 #include "monty.h"
+#include <ctype.h>
 
-/* Function to rotate the stack to the top */
+/**
+ * rotl - rotates the stack to the top.
+ * @stack: double pointer to the top of the stack
+ * @line_number: line number of the current opcode
+ * Return: 0(success)
+ */
+
 void rotl(stack_t **stack, unsigned int line_number)
 {
-    stack_t *current = *stack;
+	stack_t *current = *stack;
+	
+	if (current == NULL || current->next == NULL)
+		return;
 
-    if (current == NULL || current->next == NULL)
-        return; 
+	while (current->next != NULL)
+		current = current->next;
 
-    while (current->next != NULL)
-        current = current->next;
-
-    current->next = *stack;
-    (*stack)->prev = current;
-    *stack = (*stack)->next;
-    (*stack)->prev = NULL;
-    current->next->next = NULL;
-    (void)line_number;
+	current->next = *stack;
+	(*stack)->prev = current;
+	*stack = (*stack)->next;
+	(*stack)->prev = NULL;
+	current->next->next = NULL;
+	(void)line_number;
 }
-
