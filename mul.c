9cb67cf6255cbd43db7ec01b7784070ab03ae760
nourister <nourusterjuma@gmail.com>
2023-07-27 12:45:52 -0700

    monty

--- mul.c
+++ mul.c
@@ -1,14 +1,20 @@
 #include "monty.h"
 
-/* Function to multiply the second top element by the top element on the stack */
+/**
+ * mul - Multiplies the top two elements of the stack.
+ * @stack: Double pointer to the head of the stack.
+ * @line_number: The line number.
+ * Return: 0(success)
+ */
+
 void mul(stack_t **stack, unsigned int line_number)
 {
-    if (*stack == NULL || (*stack)->next == NULL)
-    {
-        fprintf(stderr, "L%u: can't mul, stack too short\n", line_number);
-        exit(EXIT_FAILURE);
-    }
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't mul, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 
-    (*stack)->next->n *= (*stack)->n;
-    pop(stack, line_number);
+	(*stack)->next->n *= (*stack)->n;
+	pop(stack, line_number);
 }
