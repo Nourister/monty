aa6eea5c87e1c84c79d980f94cfdfa851f47a86e
nourister <nourusterjuma@gmail.com>
2023-07-27 13:15:20 -0700

    monty

--- pstr.c
+++ pstr.c
@@ -1,16 +1,23 @@
 #include "monty.h"
 
-/* Function to print the string starting at the top of the stack */
+/**
+ * pstr - prints the string starting at the top of the stack,
+ * followed by a new line.
+ * @stack: double pointer to the top of the stack
+ * @line_number: line number of the current opcode
+ * Return: 0(success)
+ */
 void pstr(stack_t **stack, unsigned int line_number)
 {
-    stack_t *current = *stack;
+	stack_t *current = *stack;
 
-    while (current != NULL && current->n != 0 && current->n >= 0 && current->n <= 127)
-    {
-        printf("%c", current->n);
-        current = current->next;
-    }
-    printf("\n");
-    (void)line_number;
+	while (current != NULL && current->n != 0 && current->n >= 0
+	&& current->n <= 127)
+	{
+		printf("%c", current->n);
+		current = current->next;
+	}
+	printf("\n");
+	(void)line_number;
 }
 
